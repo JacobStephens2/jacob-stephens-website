c214996ff9cf0e9d956413cc7347ab8dd020fc2a
Jacob Stephens <jacob.stephens.701@gmail.com>
2023-11-07 15:28:29 -0500

    format pages and update www.jacobstephens.net links to jacobstephens.net

--- gatsby/src/components/footer.js
+++ gatsby/src/components/footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
       <div>
         © {new Date().getFullYear()}
         {` `}
-        <a href="https://www.jacobstephens.net">Jacob Stephens</a>
+        <a href="https://jacobstephens.net">Jacob Stephens</a>
         &ensp;|&ensp;
         <a href="/privacy-policy">Privacy Policy</a>
       </div>
